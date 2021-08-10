c3e03c91b18c0245904bec01e8f536c19eaa65e4
Curt Dodds <cdodds@ukirt-base-dp2.ifa.hawaii.edu>
2021-08-10 10:52:25 -1000

    Updated to support more options
    Usage: bash synth.sh step cubedir wavelength model batchtemplate
    Eg:    bash synth.sh 000000 200G 6302 200G ~/pbs/sir.pbs
    Step    6 digit simulation checkpoint/step i.e. set of subdomain_x.step files
    Cubedir path to parent dir of model dir
    Wavelength approx angstroms to specify which emissions line to synthesize
    Model e.g. 200G (implied 3D subdir contains cubes)
    Batch template file e.g. for Torgue or Slurm - see example sir.pbs

--- synth.sh
+++ synth.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 step=$1
-cubePath=${2:-.}
+cubeDir=${2:-.}
+waveLength=${3:-6302}
+model=$4
+batchTemplate=$5
+
+if test -z "$model"
+then
+  cubePath=$cubeDir
+else
+  cubePath=$cubeDir/$model/3D
+fi
 
 if test ! -e ${cubePath}/subdomain_0.${step}
 then
@@ -9,18 +19,35 @@ then
   exit 1
 fi
 
-echo Path $cubePath
-echo Step $step
+sirPath=`dirname $cubePath`/SIR
+mkdir -p $sirPath
+echo Path   $cubePath
+echo Step   $step
+echo Output $sirPath
 ls $cubePath/*.$step
 
-sed -e "s@##STEP##@$step@g" -e "s@##PATH##@$cubePath@g" <synth.ini >$cubePath/synth-$step.ini
-cat $cubePath/synth-$step.ini
+sed -e "s@##STEP##@$step@g" -e "s@##PATH##@$cubePath@g" -e "s@##WL##@$waveLength@g" -e "s@##$waveLength##@@g" -e "/##[0-9]*##/d" <synth.ini >$sirPath/synth-$step-$waveLength.ini
+echo "3D SIR Configuration"
+cat $sirPath/synth-$step-$waveLength.ini
+echo
+
+if test ! -z "$batchTemplate" && test ! -z "$model"
+then
+  batchPath=`dirname $batchTemplate`
+  batchFile=$(basename -- "$batchTemplate")
+  batchSuffix="${batchFile##*.}"
+  batchPrefix="${batchFile%.*}"
+  sed -e "s@##MODEL##@$model@g" -e "s@##STEP##@$step@g" -e "s@##PATH##@$cubePath@g" -e "s@##WL##@$waveLength@g" <$batchTemplate >$batchPath/$batchPrefix-$step-$waveLength.$batchSuffix
+  echo "Batch Submission"
+  cat $batchPath/$batchPrefix-$step-$waveLength.$batchSuffix
+  echo
+fi
 
 cat <<EOF
 
 To synthesize the entire cube run the following commands:
 
-cd $cubePath
-mpiexec -n 18 python `pwd`/synth.py --init=synth-$step.ini
+cd $sirPath
+mpiexec -n 36 python `pwd`/synth.py --batch512 --init=synth-$step-$waveLength.ini
 EOF
 
