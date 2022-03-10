d8939bb9bac2b8da56c18eb48b897b75c95c561d
Curt Dodds <dodds@hawaii.edu>
2022-03-09 20:30:41 -1000

    Add useMPI command line and internal option flag

--- synth.py
+++ synth.py
@@ -3,15 +3,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-b","--batch", type=int, default=128, help="batchsize")
-parser.add_argument("-p","--path", default='./', help="directory containing the SIR/MURaM files")
 parser.add_argument("-i","--init", default='synth.ini', help="3DSIR configuration file")
 parser.add_argument("-X","--x", type=int, default=0, help="x origin")
 parser.add_argument("-Y","--y", type=int, default=0, help="y origin")
 parser.add_argument("-W","--width", type=int, default=0, help="width")
 parser.add_argument("-H","--height", type=int, default=0, help="height")
+parser.add_argument("-m","--mpi", dest='useMPI', default=False, action='store_true', help="parallel execution with MPI")
 args = parser.parse_args()
 
-sirPath=args.path
 iniFile=args.init
 x1=args.x
 x2=x1 + args.width
@@ -19,13 +18,20 @@ y1=args.y
 y2=y1 + args.height
 batchSize=args.batch
 
-iterator = sir3d.synth.Iterator(use_mpi=True, batch=batchSize)
+print(args.useMPI)
+if args.useMPI:
+  iterator = sir3d.synth.Iterator(use_mpi=args.useMPI, batch=batchSize)
+  mod = sir3d.synth.Model(iniFile, rank=iterator.get_rank())
+  iterator.use_model(model=mod)
+else:
+  iterator = sir3d.synth.Iterator(use_mpi=args.useMPI)
+  mod = sir3d.synth.Model(iniFile, rank=iterator.get_rank())
+  iterator.use_model(model=mod)
 
-mod = sir3d.synth.Model('%s/%s'%(sirPath, iniFile), rank=iterator.get_rank())
-iterator.use_model(model=mod)
 
 if (args.width * args.height == 0):
   iterator.run_all_pixels()
 else:
+  print('running subcube')
   iterator.run_all_pixels(rangex=[x1,x2], rangey=[y1,y2])
 
